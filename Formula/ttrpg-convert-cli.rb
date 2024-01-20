2783dacb5dc8e553ca3ede3d61cf5c134eaf5c35
Erin Schnabel <erinschnabel@gmail.com>
2024-01-20 12:20:35 -0500

    🔖 2.2.17 ttrpg-convert-cli

--- Formula/ttrpg-convert-cli.rb
+++ Formula/ttrpg-convert-cli.rb
@@ -2,19 +2,19 @@
 class TtrpgConvertCli < Formula
   desc "Utility to convert 5eTools and Pf2eTools JSON data into Markdown"
   homepage "https://github.com/ebullient/ttrpg-convert-cli"
-  url "https://github.com/ebullient/ttrpg-convert-cli/releases/download/2.2.16/ttrpg-convert-cli-2.2.16-runner.jar", using: :nounzip
-  version "2.2.16"
-  sha256 "dc040ecdbe6f528cddc1ddf8b7859073d408d4a705526dccd3b762fe6ad1bd32"
+  url "https://github.com/ebullient/ttrpg-convert-cli/releases/download/2.2.17/ttrpg-convert-cli-2.2.17-runner.jar", using: :nounzip
+  version "2.2.17"
+  sha256 "f8d4d8ffff303cd2d25685225dbc7ec740fa3ad057b557632e01aec42040567b"
   license "Apache-2.0"
 
   depends_on "openjdk@17"
 
   def install
-    libexec.install "ttrpg-convert-cli-2.2.16-runner.jar"
+    libexec.install "ttrpg-convert-cli-2.2.17-runner.jar"
     bash = <<~EOS
       #!/bin/bash
       export JAVA_HOME="#{Language::Java.overridable_java_home_env(nil)[:JAVA_HOME]}"
-      exec "${JAVA_HOME}/bin/java" -jar #{libexec}/ttrpg-convert-cli-2.2.16-runner.jar "$@"
+      exec "${JAVA_HOME}/bin/java" -jar #{libexec}/ttrpg-convert-cli-2.2.17-runner.jar "$@"
     EOS
 
     bin.mkpath
@@ -23,6 +23,6 @@ class TtrpgConvertCli < Formula
 
   test do
     output = shell_output("#{bin}/ttrpg-convert --version")
-    assert_match "2.2.16", output
+    assert_match "2.2.17", output
   end
 end
