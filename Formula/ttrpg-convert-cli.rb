11564c6258a6e25b277d0f1f268f433557952d11
jreleaserbot <jreleaser@kordamp.org>
2024-07-15 21:40:43 +0000

    🔖  2.3.17 ttrpg-convert-cli

--- Formula/ttrpg-convert-cli.rb
+++ Formula/ttrpg-convert-cli.rb
@@ -1,22 +1,22 @@
-# Generated with JReleaser 1.12.0 at 2024-06-03T23:26:19.156348653Z
+# Generated with JReleaser 1.13.1 at 2024-07-15T21:40:27.851617442Z
 
 class TtrpgConvertCli < Formula
   desc "Utility to convert 5eTools and Pf2eTools JSON data into Markdown"
   homepage "https://github.com/ebullient/ttrpg-convert-cli"
-  version "2.3.16"
+  version "2.3.17"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ebullient/ttrpg-convert-cli/releases/download/2.3.16/ttrpg-convert-cli-2.3.16-linux-x86_64.zip"
-    sha256 "b6b4f83a7003277fdd9d74a3bab1dc3ab3364cd09c07e4838c19d389c832d190"
+    url "https://github.com/ebullient/ttrpg-convert-cli/releases/download/2.3.17/ttrpg-convert-cli-2.3.17-linux-x86_64.zip"
+    sha256 "235c9207327efc91dc3af66c14e2de4cc3e6aa95008ea714bd20a1a611facf47"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/ebullient/ttrpg-convert-cli/releases/download/2.3.16/ttrpg-convert-cli-2.3.16-osx-aarch_64.zip"
-    sha256 "076c709aacf492329b21ee8183949cc494f9ba4f787570af7187789b0bc5d8a0"
+    url "https://github.com/ebullient/ttrpg-convert-cli/releases/download/2.3.17/ttrpg-convert-cli-2.3.17-osx-aarch_64.zip"
+    sha256 "7a2e6276493ddc6f4e3919d495e49f35c8613ec50bc2dbf94cc11fe3278623a0"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ebullient/ttrpg-convert-cli/releases/download/2.3.16/ttrpg-convert-cli-2.3.16-osx-x86_64.zip"
-    sha256 "50dba2ab2726b82c4bf0c59fc5a84f0656f194326aa4538b08fa8a3cf0b39847"
+    url "https://github.com/ebullient/ttrpg-convert-cli/releases/download/2.3.17/ttrpg-convert-cli-2.3.17-osx-x86_64.zip"
+    sha256 "8ad640f24a6fec1d4a15bf73fe7dd94a6310efd0b026c8510707a2783d2a01c3"
   end
 
 
@@ -27,6 +27,6 @@ class TtrpgConvertCli < Formula
 
   test do
     output = shell_output("#{bin}/ttrpg-convert --version")
-    assert_match "2.3.16", output
+    assert_match "2.3.17", output
   end
 end
