027e740b6323b6c4cff588048c4f5cb433ee1f6e
jreleaserbot <jreleaser@kordamp.org>
2025-03-21 13:13:26 +0000

    🔖  3.0.2 ttrpg-convert-cli

--- Formula/ttrpg-convert-cli.rb
+++ Formula/ttrpg-convert-cli.rb
@@ -1,22 +1,22 @@
-# Generated with JReleaser 1.16.0 at 2025-01-30T11:25:54.345876261Z
+# Generated with JReleaser 1.17.0 at 2025-03-21T13:13:09.231648139Z
 
 class TtrpgConvertCli < Formula
   desc "Utility to convert 5eTools and Pf2eTools JSON data into Markdown"
   homepage "https://github.com/ebullient/ttrpg-convert-cli"
-  version "3.0.1"
+  version "3.0.2"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ebullient/ttrpg-convert-cli/releases/download/3.0.1/ttrpg-convert-cli-3.0.1-linux-x86_64.zip"
-    sha256 "5ac3b5b91a10a0476ee041ea81cf2eaf1f57d0501983288c0512085b315817b2"
+    url "https://github.com/ebullient/ttrpg-convert-cli/releases/download/3.0.2/ttrpg-convert-cli-3.0.2-linux-x86_64.zip"
+    sha256 "37a319373f0c26c101348d6466662df3b8f295d685f5c04202dbee25d9b2188f"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/ebullient/ttrpg-convert-cli/releases/download/3.0.1/ttrpg-convert-cli-3.0.1-osx-aarch_64.zip"
-    sha256 "4f2c288d361afbfee59d79a26555bee21053317a2791901c67bdd05c348d2dfe"
+    url "https://github.com/ebullient/ttrpg-convert-cli/releases/download/3.0.2/ttrpg-convert-cli-3.0.2-osx-aarch_64.zip"
+    sha256 "0a79b806331988c9595b5a1bb6d7cd41182d675ec9c86b12fbec5599353cb549"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ebullient/ttrpg-convert-cli/releases/download/3.0.1/ttrpg-convert-cli-3.0.1-osx-x86_64.zip"
-    sha256 "202a27203095864ce7075d956d5a6d2133b52db564be61be0472ecead4322f11"
+    url "https://github.com/ebullient/ttrpg-convert-cli/releases/download/3.0.2/ttrpg-convert-cli-3.0.2-osx-x86_64.zip"
+    sha256 "cee0ca066fd0f8d082d50668a5506a044a851c9371e0a666f2f34bf304f290b2"
   end
 
 
@@ -27,6 +27,6 @@ class TtrpgConvertCli < Formula
 
   test do
     output = shell_output("#{bin}/ttrpg-convert --version")
-    assert_match "3.0.1", output
+    assert_match "3.0.2", output
   end
 end
